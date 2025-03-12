cdf72d28e100fb868509aaf5153cd32debc2403e
Olivier Louvignes <olivier@mgcrea.io>
2025-03-12 23:57:20 +0100

    chore(SwiftUIRootView): split props away

---
+++ ios/SwiftUIRootProps.swift
@@ -0,0 +1,74 @@
+import SwiftUI
+
+final class SwiftUIRootProps: ObservableObject {
+  @Published private(set) var viewTree: (any SwiftUINode)?
+  @Published var pickerSelections: [String: String] = [:]
+  @Published var dateSelections: [String: Date] = [:]
+  @Published var textFieldValues: [String: String] = [:]
+  public var onEvent: ((String, String, String, String?) -> Void)?
+
+  func update(with newDictionary: [String: Any]) {
+    if let jsonString = newDictionary["viewTree"] as? String {
+      do {
+        viewTree = try decodeSwiftUINode(from: jsonString)
+        bindEventHandlers(from: viewTree)
+      } catch {
+        print("Failed to decode view tree: \(error)")
+        viewTree = nil
+      }
+    }
+  }
+
+  private func bindEventHandlers(from node: (any SwiftUINode)?) {
+    guard let node = node else { return }
+    if let picker = node as? GenericNode<PickerProps> {
+      picker.props.onChange = { [weak self] value in
+        self?.onEvent?("change", "Picker", picker.id, value)
+      }
+    } else if let stepper = node as? GenericNode<StepperProps> {
+      stepper.props.onChange = { [weak self] value in
+        self?.onEvent?("change", "Stepper", stepper.id, value)
+      }
+    } else if let datePicker = node as? GenericNode<DatePickerProps> {
+      datePicker.props.onChange = { [weak self] rawValue in
+        let value = ISO8601DateFormatter().string(from: rawValue)
+        self?.onEvent?("change", "DatePicker", datePicker.id, value)
+      }
+    } else if let textField = node as? GenericNode<TextFieldProps> {
+      textField.props.onChange = { [weak self] value in
+        self?.onEvent?("change", "TextField", textField.id, value)
+      }
+      textField.props.onFocus = { [weak self] in
+        self?.onEvent?("focus", "TextField", textField.id, nil)
+      }
+      textField.props.onBlur = { [weak self] in
+        self?.onEvent?("blur", "TextField", textField.id, nil)
+      }
+    } else if let button = node as? GenericNode<ButtonProps> {
+      button.props.onPress = { [weak self] in
+        self?.onEvent?("press", "Button", button.id, nil)
+      }
+    } else if let toggle = node as? GenericNode<ToggleProps> {
+      toggle.props.onChange = { [weak self] value in
+        self?.onEvent?("change", "Toggle", toggle.id, String(value))
+      }
+    } else if let slider = node as? GenericNode<SliderProps> {
+      slider.props.onChange = { [weak self] value in
+        self?.onEvent?("change", "Slider", slider.id, String(value))
+      }
+      slider.props.onFocus = { [weak self] in
+        self?.onEvent?("focus", "Slider", slider.id, nil)
+      }
+      slider.props.onBlur = { [weak self] in
+        self?.onEvent?("blur", "Slider", slider.id, nil)
+      }
+    } else if let sheet = node as? GenericNode<SheetProps> {
+      sheet.props.onDismiss = { [weak self] in
+        self?.onEvent?("dismiss", "Sheet", sheet.id, nil)
+      }
+    }
+    if let children = node.children {
+      children.forEach { bindEventHandlers(from: $0) }
+    }
+  }
+}
--- ios/SwiftUIRootView.swift
+++ ios/SwiftUIRootView.swift
@@ -1,89 +1,14 @@
-// ios/SwiftUIRootView.swift
 import Combine
 import SwiftUI
-import UIKit
-
-final class ContainerProps: ObservableObject {
-  @Published private(set) var viewTree: (any SwiftUINode)?
-  @Published var pickerSelections: [String: String] = [:]
-  @Published var dateSelections: [String: Date] = [:]
-  @Published var textFieldValues: [String: String] = [:]
-  public var onEvent: ((String, String, String, String?) -> Void)?
-
-  func update(with newDictionary: [String: Any]) {
-    if let jsonString = newDictionary["viewTree"] as? String {
-      do {
-        viewTree = try decodeSwiftUINode(from: jsonString)
-        bindEventHandlers(from: viewTree)
-      } catch {
-        print("Failed to decode view tree: \(error)")
-        viewTree = nil
-      }
-    }
-  }
-
-  private func bindEventHandlers(from node: (any SwiftUINode)?) {
-    guard let node = node else { return }
-    if let picker = node as? GenericNode<PickerProps> {
-      picker.props.onChange = { [weak self] value in
-        self?.onEvent?("change", "Picker", picker.id, value)
-      }
-    } else if let stepper = node as? GenericNode<StepperProps> {
-      stepper.props.onChange = { [weak self] value in
-        self?.onEvent?("change", "Stepper", stepper.id, value)
-      }
-    } else if let datePicker = node as? GenericNode<DatePickerProps> {
-      datePicker.props.onChange = { [weak self] rawValue in
-        let value = ISO8601DateFormatter().string(from: rawValue)
-        self?.onEvent?("change", "DatePicker", datePicker.id, value)
-      }
-    } else if let textField = node as? GenericNode<TextFieldProps> {
-      textField.props.onChange = { [weak self] value in
-        self?.onEvent?("change", "TextField", textField.id, value)
-      }
-      textField.props.onFocus = { [weak self] in
-        self?.onEvent?("focus", "TextField", textField.id, nil)
-      }
-      textField.props.onBlur = { [weak self] in
-        self?.onEvent?("blur", "TextField", textField.id, nil)
-      }
-    } else if let button = node as? GenericNode<ButtonProps> {
-      button.props.onPress = { [weak self] in
-        self?.onEvent?("press", "Button", button.id, nil)
-      }
-    } else if let toggle = node as? GenericNode<ToggleProps> {
-      toggle.props.onChange = { [weak self] value in
-        self?.onEvent?("change", "Toggle", toggle.id, String(value))
-      }
-    } else if let slider = node as? GenericNode<SliderProps> {
-      slider.props.onChange = { [weak self] value in
-        self?.onEvent?("change", "Slider", slider.id, String(value))
-      }
-      slider.props.onFocus = { [weak self] in
-        self?.onEvent?("focus", "Slider", slider.id, nil)
-      }
-      slider.props.onBlur = { [weak self] in
-        self?.onEvent?("blur", "Slider", slider.id, nil)
-      }
-    } else if let sheet = node as? GenericNode<SheetProps> {
-      sheet.props.onDismiss = { [weak self] in
-        self?.onEvent?("dismiss", "Sheet", sheet.id, nil)
-      }
-    }
-    if let children = node.children {
-      children.forEach { bindEventHandlers(from: $0) }
-    }
-  }
-}
 
 @objc(SwiftUIRootView)
 public class SwiftUIRootView: SwiftUIContainerView {
-  @ObservedObject private var props: ContainerProps
+  @ObservedObject private var props: SwiftUIRootProps
   private var cancellables: Set<AnyCancellable> = []
   @objc public var onEvent: ((NSString, NSString, NSString, NSString?) -> Void)?
 
   @objc public init(frame _: CGRect) {
-    props = ContainerProps()
+    props = SwiftUIRootProps()
     super.init(rootView: AnyView(EmptyView()))
 
     props.onEvent = { [weak self] name, type, id, value in
