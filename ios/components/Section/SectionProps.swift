618fdcb2fabaaa57dd579a38bb8403ecb9fea50f
Olivier Louvignes <olivier@mgcrea.io>
2025-03-27 23:42:24 +0100

    chore(Section): format files

--- ios/components/Section/SectionProps.swift
+++ ios/components/Section/SectionProps.swift
@@ -1,24 +1,31 @@
 import SwiftUI
 
 public final class SectionProps: ObservableObject, Decodable {
-    @Published public var header: String
-    @Published public var footer: String
-    @Published public var isCollapsed: Bool
+//  @Published public var header: [any SwiftUINode]?
+//  @Published public var footer: [any SwiftUINode]?
+  @Published public var header: String
+  @Published public var footer: String
+  @Published public var isCollapsed: Bool
+  @Published public var style: StyleProps?
+  
+  enum CodingKeys: String, CodingKey {
+    case header, footer, isCollapsed, style
+  }
 
-    enum CodingKeys: String, CodingKey {
-        case header, footer, isCollapsed
-    }
+  public required init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+//    header = try container.decodeIfPresent(NodeChildren.self, forKey: .header)?.nodes
+//    footer = try container.decodeIfPresent(NodeChildren.self, forKey: .footer)?.nodes
+    header = try container.decodeIfPresent(String.self, forKey: .header) ?? ""
+    footer = try container.decodeIfPresent(String.self, forKey: .footer) ?? ""
+    isCollapsed = try container.decodeIfPresent(Bool.self, forKey: .isCollapsed) ?? false
+    style = try container.decodeIfPresent(StyleProps.self, forKey: .style)
+  }
 
-    public required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        header = try container.decodeIfPresent(String.self, forKey: .header) ?? ""
-        footer = try container.decodeIfPresent(String.self, forKey: .footer) ?? ""
-        isCollapsed = try container.decodeIfPresent(Bool.self, forKey: .isCollapsed) ?? false
-    }
-
-    public func merge(from other: SectionProps) {
-        header = other.header
-        footer = other.footer
-        isCollapsed = other.isCollapsed
-    }
+  public func merge(from other: SectionProps) {
+    header = other.header
+    footer = other.footer
+    isCollapsed = other.isCollapsed
+    style = other.style
+  }
 }
--- ios/components/Section/SectionView.swift
+++ ios/components/Section/SectionView.swift
@@ -2,24 +2,25 @@
 import SwiftUI
 
 public struct SectionView<Content: View>: View {
-    @ObservedObject public var props: SectionProps
-    let content: () -> Content
+  @ObservedObject public var props: SectionProps
+  let content: () -> Content
 
-    public init(props: SectionProps, @ViewBuilder content: @escaping () -> Content) {
-        self.props = props
-        self.content = content
-    }
+  public init(props: SectionProps, @ViewBuilder content: @escaping () -> Content) {
+    self.props = props
+    self.content = content
+  }
 
-    public var body: some View {
-        Section(
-            header: props.header.isEmpty ? nil : Text(props.header),
-            footer: props.footer.isEmpty ? nil : Text(props.footer)
-        ) {
-            if !props.isCollapsed {
-                content()
-            } else {
-                EmptyView() // Hide content when collapsed
-            }
-        }
+  public var body: some View {
+    Section(
+      header: props.header.isEmpty ? nil : Text(props.header),
+      footer: props.footer.isEmpty ? nil : Text(props.footer)
+    ) {
+      if !props.isCollapsed {
+        content()
+      } else {
+        EmptyView() // Hide content when collapsed
+      }
     }
+    .applyViewStyles(props.style)
+  }
 }
