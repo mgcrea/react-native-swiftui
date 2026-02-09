69430f86256b2a7aafdc231785315beb6476407b
Olivier Louvignes <olivier@mgcrea.io>
2026-02-09 10:35:34 +0100

    style: format useNormalizedStyles to single line

--- src/hooks/useNormalizedStyles.ts
+++ src/hooks/useNormalizedStyles.ts
@@ -5,8 +5,5 @@ import type { NativeTextStyle, NativeViewStyle } from "src/types";
 export const useNormalizedStyles = <T extends NativeTextStyle | NativeViewStyle>(
   style?: StyleProp<T>,
 ): T | undefined => {
-  return useMemo(
-    () => (style != null ? StyleSheet.flatten<T>(style) : undefined),
-    [style],
-  ) as T | undefined;
+  return useMemo(() => (style != null ? StyleSheet.flatten<T>(style) : undefined), [style]) as T | undefined;
 };
